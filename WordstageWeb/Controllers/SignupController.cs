62d2347c79b04007c943cf7cfd56dea2a34e0210
Jay <jaybhatt.dcs@gmail.com>
2023-04-12 15:11:42 +0530

    Mail Functionality is resolved  after the registration is successful.

--- WordstageWeb/Controllers/SignupController.cs
+++ WordstageWeb/Controllers/SignupController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using MimeKit;
+using MailKit.Net.Smtp;
 using WordstageWeb.Models;
 using WordstageWeb.Repository;
 using WordstageWeb.Services;
@@ -31,7 +33,40 @@ namespace WordstageWeb.Controllers
                 var Userinfo = await _signUpRepository.SignUp(firstname, lastname, emailAddress, password, usertype);
                 if (Userinfo)
                 {
-                    Console.WriteLine("Succesfully register");
+                    string Body;
+                    MimeMessage emailMessage = new MimeMessage();
+
+                    MailboxAddress emailFrom = new MailboxAddress("Jay", "jaybhatt.dcs@gmail.com");
+                    emailMessage.From.Add(emailFrom);
+
+                    MailboxAddress emailTo = new MailboxAddress(firstname, emailAddress);
+                    emailMessage.To.Add(emailTo);
+
+                    emailMessage.Subject = "Welcome To Worsdtag";
+                    string FilePath = Directory.GetCurrentDirectory() + "~\\MailHtml\\WelcomeEmail.html";
+                   // string EmailTemplateText = File.ReadAllText(FilePath)
+
+                    /*EmailTemplateText = string.Format(EmailTemplateText, firstname, password, DateTime.Now.Date.ToShortDateString());
+                    EmailTemplateText = EmailTemplateText.Replace("{0}", firstname);
+                    EmailTemplateText = EmailTemplateText.Replace("{1}", password.ToString());*/
+
+                    BodyBuilder emailBodyBuilder = new BodyBuilder();
+                    //emailBodyBuilder.HtmlBody = EmailTemplateText;
+                    emailBodyBuilder.HtmlBody = "<br/> Your registration completed succesfully.";
+
+                    emailMessage.Body = emailBodyBuilder.ToMessageBody();
+
+                    SmtpClient emailClient = new SmtpClient();
+                    //emailClient.Connect(_emailSettings.Host, _emailSettings.Port, _emailSettings.UseSSL);
+                    //emailClient.Authenticate(_emailSettings.EmailId, _emailSettings.Password);
+                    emailClient.Connect("smtp.gmail.com", 465, true);
+                    emailClient.Authenticate("jaybhatt.dcs@gmail.com", "yxmbefvzfqoybkvl");
+                    emailClient.Send(emailMessage);
+                    emailClient.Disconnect(true);
+                    emailClient.Dispose();
+
+                    var message = "Registration Completed. Please check your Email:" + emailAddress;
+                    ViewBag.Message = message;
                     return RedirectToAction("Login", "Login");
                     //return View();
                 }
