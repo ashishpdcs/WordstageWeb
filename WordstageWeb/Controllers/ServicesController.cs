01ae844996c5c8fc14c5618ab9a40c8d8256a766
Jay <jaybhatt.dcs@gmail.com>
2023-04-13 14:34:57 +0530

    Design the sevice page and some changes in homepage is resolved.

---
+++ WordstageWeb/Controllers/ServicesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WordstageWeb.Controllers
+{
+    public class ServicesController : Controller
+    {
+        public IActionResult Services()
+        {
+            return View();
+        }
+    }
+}
